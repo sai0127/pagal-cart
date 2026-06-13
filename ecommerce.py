5fcdfb76b2612ca900d2a5d8dcf1261b6709c00a
sai0127 <buravanchamohansai@gmail.com>
2026-06-13 11:26:41 +0530

    fix mail port to 465

--- ecommerce.py
+++ ecommerce.py
@@ -19,12 +19,12 @@ app.config['JWT_SECRET_KEY'] = 'your-secret-key'
 jwt = JWTManager(app)
 # mail config
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_EMAIL')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 mail = Mail(app)
-
 # ─── DATABASE CONNECTION ───────────────────────────────────────────────────────
 
 def get_db():
