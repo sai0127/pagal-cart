cb063967134a4e8b878c8ed3c703c56d1af330a9
sai0127 <buravanchamohansai@gmail.com>
2026-06-17 11:40:58 +0530

    add try demo button on landing page

--- static/js/landing.js
+++ static/js/landing.js
@@ -1,3 +1,18 @@
 if (localStorage.getItem('token')) {
     window.location.href = '/shop';
 }
+async function tryDemo() {
+    let response = await fetch('https://pagal-cart.onrender.com/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email: 'demo@pagalcart.com', password: 'demo1234' })
+    });
+    let data = await response.json();
+    if (data.token) {
+        localStorage.setItem('token', data.token);
+        localStorage.setItem('user_id', data.id);
+        localStorage.setItem('role', data.role);
+        localStorage.setItem('user_name', data.name);
+        window.location.href = '/shop';
+    }
+}
